ede8eedb5c0af585acbdcc5b447372e2b9a0f3be
wangning <l>
2020-11-09 17:40:13 +0800

    🎉 router

---
+++ src/router/common.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    path: '/home',
+    name: 'name',
+    component: () => import('@/views/Home.vue'),
+    meta: {
+      title: '首页',
+      keepAlive: false
+    }
+  }
+]
