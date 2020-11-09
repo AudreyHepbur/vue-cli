b4116e6b3c6dca154dbb7f25e907258a7bcb12d9
wangning <l>
2020-11-09 17:40:13 +0800

    🎉 router
    🚀 热更新
    
    🎉 jest单元测试

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,37 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
-// 自动化注册路由
-const routerContext = require.context('./', true, /\.js$/)
-routerContext.keys().forEach(fileName => {
-  // 获取组件配置
-  const componentConfig = routerContext(fileName)
-})
 
-const routes = [
+let routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
-  },
+    name: "home",
+    component: Home,
+    meta: {
+      title: "空白页",
+      navHide: true,
+      auth: true,
+      keepAlive: true
+    }
+  }
 ];
+// 自动化注册路由
+const routerContext = require.context("./", true, /\.js$/);
+routerContext.keys().forEach(fileName => {
+  // 如果是根目录的 index.js 、不处理
+  if (fileName.startsWith("./index")) {
+    return;
+  }
+  // 获取组件配置
+  const componentConfig = routerContext(fileName);
+  /**
+   * 兼容 import export 和 require module.export 两种规范
+   */
+  console.log("componentConfig :>> ", componentConfig);
+  routes = routes.concat(componentConfig.default || routerModule);
+});
 
 const router = new VueRouter({
   routes
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,66 @@
 <template>
   <div>
-    开发
+    <!-- S 宣讲行程时间轴 -->
+    <div class="timeline">
+      <div class="timeline_tit">
+        宣讲行程
+        <span>Campus Talk Schedule</span>
+      </div>
+      <div class="timeline_icon">
+        <svg viewBox="0 0 102 102">
+          <circle cx="51" cy="51" r="50" transform="rotate(90 51 51)"></circle>
+        </svg>
+      </div>
+      <ul class="timeline_list">
+        <li class="timeline_item timeline_item_sp">
+          <span class="timeline_item_cnt flag"><i class="flag_sin"></i></span>
+          <span class="timeline_item_cnt country"></span>
+          <b></b>
+        </li>
+        <li class="timeline_item">
+          <span class="timeline_item_cnt time">09 / 20</span>
+          <span class="timeline_item_cnt city">INSEAD</span>
+          <b></b>
+        </li>
+        <li class="timeline_item">
+          <span class="timeline_item_cnt time">09 / 21</span>
+          <span class="timeline_item_cnt city">NTU</span>
+          <b></b>
+        </li>
+        <li class="timeline_item timeline_item_sp">
+          <span class="timeline_item_cnt flag"><i class="flag_us"></i></span>
+          <span class="timeline_item_cnt country">U.S</span>
+          <b></b>
+        </li>
+        <li class="timeline_item">
+          <span class="timeline_item_cnt time">09 / 26</span>
+          <span class="timeline_item_cnt city">UCLA</span>
+          <b></b>
+        </li>
+        <li class="timeline_item">
+          <span class="timeline_item_cnt time">09 / 28</span>
+          <span class="timeline_item_cnt city">UC Berkely</span>
+          <b></b>
+        </li>
+        <li class="timeline_item">
+          <span class="timeline_item_cnt time">10 / 05</span>
+          <span class="timeline_item_cnt city">Wharton</span>
+          <b></b>
+        </li>
+        <li class="timeline_item timeline_item_sp">
+          <span class="timeline_item_cnt flag"><i class="flag_uk"></i></span>
+          <span class="timeline_item_cnt country">U.K</span>
+          <b></b>
+        </li>
+        <li class="timeline_item">
+          <span class="timeline_item_cnt time">10 / 29</span>
+          <span class="timeline_item_cnt city">LBS</span>
+          <b></b>
+        </li>
+        <li class="timeline_line"></li>
+      </ul>
+    </div>
+    <!-- E 宣讲行程时间轴 -->
   </div>
 </template>
 
@@ -9,16 +69,16 @@ export default {
   name: "",
   components: {},
   props: {},
-  data () {
+  data() {
     return {};
   },
-  created () { },
-  mounted () {
-    console.log('this.store :>> ', this.$store);
+  created() {},
+  mounted() {
+    console.log("this.store :>> ", this.$store);
   },
-  activited () { },
-  update () { },
-  beforeRouteUpdate () { },
+  activited() {},
+  update() {},
+  beforeRouteUpdate() {},
   methods: {},
   filter: {},
   computed: {},
@@ -27,4 +87,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+svg {
+  // 设置描边
+  stroke: #0084ff;
+  stroke-width: 2px;
+  fill: none;
+}
 </style>
