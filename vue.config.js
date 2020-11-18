ca8e95b9ca9223da282763b491f6aef5360e8e65
wangning <l>
2020-11-18 17:33:36 +0800

    🚀 热更新

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-  lintOnSave: process.env.NODE_ENV === 'development'
+  lintOnSave: process.env.NODE_ENV === 'development',  //eslint检查
 }
